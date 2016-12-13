3453ae01ef14dc70019b98b878077ac0c2a31230
jacobproffer <jacobhardingproffer@gmail.com>
2016-12-13 18:45:01 -0500

    Update scripts

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,16 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if (hourNow > 18) {
-  greeting = 'Good evening';
-} else if (hourNow >= 12) {
-  greeting = 'Good afternoon';
-} else {
-  greeting = 'Good morning';
-}
-
-function updateMessage() {
+function message() {
+  if (hourNow > 17) {
+    greeting = 'Good evening';
+  } else if (hourNow > 11) {
+    greeting = 'Good afternoon';
+  } else {
+    greeting = 'Good morning';
+  }
   var el = document.querySelector('span');
   el.textContent = greeting;
 }
 
-updateMessage();
+message();
