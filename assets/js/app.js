d9c56736727865602da96c6fde6ca8297cc71856
jacobproffer <jacobhardingproffer@gmail.com>
2016-12-19 21:36:57 -0500

    Work on refactoring js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,13 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
+const today = new Date();
+const hourNow = today.getHours();
 function message() {
+  var greeting = 'Good morning';
   if (hourNow > 17) {
     greeting = 'Good evening';
   } else if (hourNow > 11) {
     greeting = 'Good afternoon';
-  } else {
-    greeting = 'Good morning';
   }
-  var el = document.querySelector('span');
+  const el = document.querySelector('span');
   el.textContent = greeting;
 }
-
 message();
