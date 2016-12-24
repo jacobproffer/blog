72368724589f25f829e5263de98f56170cdbd6ca
jacobproffer <jacobhardingproffer@gmail.com>
2016-12-23 20:55:17 -0500

    Add new background selector

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,17 @@
 const today = new Date();
 const hourNow = today.getHours();
+const body = document.querySelector('body');
 function message() {
   var greeting = 'Good morning';
   if (hourNow > 17) {
     greeting = 'Good evening';
+    body.classList.add('night');
   } else if (hourNow > 11) {
     greeting = 'Good afternoon';
+    body.classList.add('day');
   } else {
     greeting = 'Good morning';
+    body.classList.add('morning');
   }
   const el = document.querySelector('span');
   el.textContent = greeting;
