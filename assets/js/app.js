4a5cbbda467faa5046123c0e8b43436ffe8c5ea1
jacobproffer <jacobhardingproffer@gmail.com>
2016-12-23 21:02:23 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const today = new Date();
 const hourNow = today.getHours();
 const body = document.querySelector('body');
+var greeting;
 function message() {
-  var greeting = 'Good morning';
   if (hourNow > 17) {
     greeting = 'Good evening';
     body.classList.add('night');
