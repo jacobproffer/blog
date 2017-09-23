a52ac988fac443d61924c4d4dd4e9b9fb913f2c5
jacobproffer <jacobhardingproffer@gmail.com>
2017-09-22 23:37:25 -0400

    Add social section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,4 @@ window.sr = ScrollReveal({ reset: true });
 sr.reveal('.hero-heading', { duration: 1000, scale: 1 });
 sr.reveal('.info', { duration: 1000, origin: 'right' });
 sr.reveal('.work-row', { duration: 1000, origin: 'left' });
+sr.reveal('.social', { duration: 1000, origin: 'right' });
