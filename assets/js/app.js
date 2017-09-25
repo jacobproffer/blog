145649621b508fd9b8ea4dfed4272bd8af72271b
jacobproffer <jacobhardingproffer@gmail.com>
2017-09-24 20:57:49 -0400

    Add contact section

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,4 @@ sr.reveal('.hero-heading', { duration: 1000, scale: 1 });
 sr.reveal('.info', { duration: 1000, origin: 'right' });
 sr.reveal('.work-row', { duration: 1000, origin: 'left' });
 sr.reveal('.social', { duration: 1000, origin: 'right' });
+sr.reveal('.contact', { duration: 1000, origin: 'left' });
