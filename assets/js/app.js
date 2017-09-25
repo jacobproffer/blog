b855421e185db9261ab112d9a26c1be9d833578a
jacobproffer <jacobhardingproffer@gmail.com>
2017-09-24 21:19:19 -0400

    Work on sass and comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// ScrollReveal calls
+
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.hero-heading', { duration: 1000, scale: 1 });
 sr.reveal('.info', { duration: 1000, origin: 'right' });
