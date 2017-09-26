b3fd27c3ce4208ed71688b761cf036a2704a4ec0
jacobproffer <jacobhardingproffer@gmail.com>
2017-09-25 22:06:46 -0400

    Update variable name

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,10 @@
 // Array of sections
-var elements =
-[
-  '.hero-heading',
-  '.info',
-  '.work-row',
-  '.social',
-  '.contact'
-];
+var sections = ['.hero-heading', '.info', '.work-row', '.social', '.contact'];
 
 // Set reset to true for ScrollReveal
 window.sr = ScrollReveal({ reset: true });
 
 // Loop over array of sections
-$.each(elements, function(i, val) {
+$.each(sections, function(i, val) {
   sr.reveal(val, { duration: 1000, origin: 'left', scale: 1 });
 });
