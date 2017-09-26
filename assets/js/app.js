bbb347e6a0582e495fdd5fb367b349f70560493a
jacobproffer <jacobhardingproffer@gmail.com>
2017-09-25 21:58:48 -0400

    Shorten script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
-// ScrollReveal calls
+// Array of sections
+var elements =
+[
+  '.hero-heading',
+  '.info',
+  '.work-row',
+  '.social',
+  '.contact'
+];
 
+// Set reset to true for ScrollReveal
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.hero-heading', { duration: 1000, scale: 1 });
-sr.reveal('.info', { duration: 1000, origin: 'right' });
-sr.reveal('.work-row', { duration: 1000, origin: 'left' });
-sr.reveal('.social', { duration: 1000, origin: 'right' });
-sr.reveal('.contact', { duration: 1000, origin: 'left' });
+
+// Loop over array of sections
+$.each(elements, function(i, val) {
+  sr.reveal(val, { duration: 1000, origin: 'left', scale: 1 });
+});
