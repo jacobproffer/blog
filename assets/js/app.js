e9aa7748556d360decfe46287e0a3458a5416bbc
jacobproffer <jacobhardingproffer@gmail.com>
2017-11-08 19:27:52 -0500

    Work on blog and header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,7 @@
 // Variables
-var body = $('html, body'),
-		mobileMenu = $('.mobile-nav-button'),
-		mainNavigation = $('.main-nav'),
-		mainHeader = $('.main-header'),
-		links = $('a[href*="#"]'),
-		headerHeight = mainHeader.outerHeight(),
-    sections = ['.info', '.work'];
+var mainHeader = $('.main-header'),
+    headerHeight = mainHeader.outerHeight(),
+    sections = ['main'];
 
 // Set reset to true for ScrollReveal
 window.sr = ScrollReveal({ reset: true });
@@ -15,26 +11,9 @@ $.each(sections, function(i, val) {
   sr.reveal(val, { duration: 1000, origin: 'left', scale: 1 });
 });
 
-// Mobile menu function
-mobileMenu.click(function() {
-	mainNavigation.toggleClass('nav-open');
-	$(this).toggleClass('navOpen');
-	mainHeader.toggleClass('open');
-	body.toggleClass('body-modal-open');
-	body.toggleClass('disable-scrolling');
-});
-
-links.click(function() {
-	mainNavigation.removeClass('nav-open');
-	mainHeader.removeClass('open');
-	mobileMenu.removeClass('navOpen');
-	body.removeClass('body-modal-open');
-	body.removeClass('disable-scrolling');
-});
-
 // Headroom.js settings and functions
 mainHeader.headroom({
-  offset    : headerHeight,
+  offset    : 0,
   tolerance   : 0,
   classes : {
     pinned   : "pinned",
@@ -52,21 +31,3 @@ mainHeader.headroom({
     mainHeader.removeClass('pinned');
   }
 });
-
-/*
-	 Removes ability to scroll background
-	 content on iOS when a modal is open.
-*/
-document.ontouchmove = function ( event ) {
-	var isTouchMoveAllowed = true, target = event.target;
-	while ( target !== null ) {
-    if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
-	    isTouchMoveAllowed = false;
-	    break;
-    }
-	  target = target.parentNode;
-	}
-	if ( !isTouchMoveAllowed ) {
-	  event.preventDefault();
-	}
-};
