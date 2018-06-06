60305d6fe8f1d063c3b9110a7b44661c85740d4d
jacobproffer <jacobhardingproffer@gmail.com>
2018-06-06 09:26:47 -0400

    Add zoom to background

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,43 @@
 // Variables
-var mainHeader = $('.main-header'),
-    headerHeight = mainHeader.outerHeight();
+var mainHeader = $(".main-header");
+var headerHeight = mainHeader.outerHeight();
+var background = $(".main-header-background");
 
 // Headroom.js settings and functions
 mainHeader.headroom({
-  offset    : 0,
-  tolerance   : 0,
-  classes : {
-    pinned   : "pinned",
-    unpinned : "unpinned",
-    top      : "onTop",
-    bottom   : "onBottom",
-    notTop   : "scrolled"
+  offset: 0,
+  tolerance: 0,
+  classes: {
+    pinned: "pinned",
+    unpinned: "unpinned",
+    top: "onTop",
+    bottom: "onBottom",
+    notTop: "scrolled"
   },
-	onUnpin : function() {
-		if ( mainHeader.hasClass('open') ) {
-			mainHeader.removeClass('unpinned');
-		}
-	},
-  onTop : function() {
-    mainHeader.removeClass('pinned');
+  onUnpin: function() {
+    if (mainHeader.hasClass("open")) {
+      mainHeader.removeClass("unpinned");
+    }
+  },
+  onTop: function() {
+    mainHeader.removeClass("pinned");
   }
 });
 
 // Circle Type
 
-new CircleType(document.getElementById('curve')).radius(400);
+new CircleType(document.getElementById("curve")).radius(400);
+
+// Header
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll < 1000) {
+    scroll = $(window).scrollTop();
+  } else {
+    scroll = 1000;
+  }
+  background.css({
+    transform: "scale(" + (100 + scroll / 5) / 100 + ")"
+  });
+});
