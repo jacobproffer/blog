b293df4081a1ee9af93c1601e2d691377784375f
Jacob Proffer <jacobhardingproffer@gmail.com>
2019-09-14 17:50:53 -0400

    Netlify test (#170)
    * Config test
    
    * Test

--- themes/jacobproffer/assets/js/app.js
+++ themes/jacobproffer/assets/js/app.js
@@ -26,3 +26,13 @@ var headroom = new Headroom(mainHeader, {
 });
 
 headroom.init();
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
