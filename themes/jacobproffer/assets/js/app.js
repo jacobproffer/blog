39ba86c2dbd4cfb18ad02fda54ffb1af5bc9f205
jacobproffer <jacobhardingproffer@gmail.com>
2022-12-18 12:53:23 -0500

    Add gsap

---
+++ themes/jacobproffer/assets/js/app.js
@@ -0,0 +1,27 @@
+const mainHeader = document.querySelector(".main-header");
+const fadeIns = document.querySelectorAll('.gsap-fade-in');
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: 'main',
+  start: 'top top',
+  end: 'max',
+  onEnter: () => mainHeader.classList.add('main-header--pinned'),
+  onLeaveBack: () => mainHeader.classList.remove('main-header--pinned'),
+});
+
+if (fadeIns.length > 0) {
+  fadeIns.forEach((fadeIn) => {
+    gsap.from(fadeIn, {
+      y: 30,
+      autoAlpha: 0,
+      scrollTrigger: {
+        trigger: fadeIn,
+        start: 'top 90%',
+        end: 'bottom top',
+        once: true,
+      }
+    })
+  });
+}
